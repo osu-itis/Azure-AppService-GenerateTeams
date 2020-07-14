db554fb390213a7ebee1637404d9d7c3455efb8e
Keenan Carr <keenan.carr@oregonstate.edu>
2020-07-13 17:09:14 -0700

    Updated draft and added config info to json files

--- HttpTrigger/run.ps1
+++ HttpTrigger/run.ps1
@@ -6,23 +6,10 @@ param($Request, $TriggerMetadata)
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
 
-# Interact with query parameters or the body of the request.
-$name = $Request.Query.Name
-if (-not $name) {
-    $name = $Request.Body.Name
-}
-
-if ($name) {
-    $status = [HttpStatusCode]::OK
-    $body = "Hello $name"
-}
-else {
-    $status = [HttpStatusCode]::BadRequest
-    $body = "Please pass a name on the query string or in the request body."
-}
+Push-OutputBinding -Name "outputQueueItem" -Value $Request.Body
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    StatusCode = $status
-    Body = $body
+    StatusCode = [HttpStatusCode]::OK
+    Body = $($Request.Body|convertto-Json)
 })
--- QueueTrigger/run.ps1
+++ QueueTrigger/run.ps1
@@ -1,6 +1,79 @@
 # Input bindings are passed in via param block.
-param([string] $QueueItem, $TriggerMetadata)
+param($QueueItem, $TriggerMetadata)
 
-# Write out the queue message and insertion time to the information log.
-Write-Host "PowerShell queue trigger function processed work item: $QueueItem"
-Write-Host "Queue item insertion time: $($TriggerMetadata.InsertionTime)"
+#Creating a new variable as our temp object (as a hash table)
+[hashtable]$TempObject = $QueueItem
+
+#Write-Host "PowerShell queue trigger function processed work item: $($QueueItem.Keys|convertto-json)"
+#Write-Host "Queue item insertion time: $($TriggerMetadata.InsertionTime)"
+
+#Fixing any email addresses that improperly formatted in the hashtable
+$TempObject.TeamOwner = $TempObject.TeamOwner.tostring().replace("%40", "@")
+$TempObject.Requestor = $TempObject.Requestor.tostring().replace("%40", "@")
+
+#Creating the needed credentials to access Teams:
+$ServiceAccountCredentials = New-Object System.Management.Automation.PSCredential (
+    "$ENV:ServiceAccountUsername",
+    $(ConvertTo-SecureString "$($ENV:ServiceAccountPassword)" -AsPlainText -Force)
+)
+
+#LOOKS LIKE STUFF IS BROKEN IN POWERSHELL CORE FOR AZURE FUNCTIONS ^^^^^^^^ https://github.com/MicrosoftDocs/office-docs-powershell/issues/5950
+
+
+# $ServiceAccountCredentials = New-Object System.Management.Automation.PSCredential (
+#    "$ENV:ServiceAccountUsername",
+#    $(ConvertTo-SecureString "$((Get-AzKeyVaultSecret -vaultName "TeamsAutomationKeyVault" -name "TeamsAutomationSecret").SecretValueText)" -AsPlainText -Force)
+# )
+
+#function to format and create the new MS team
+function New-MSTeams {
+    [CmdletBinding()]
+    PARAM (
+        [parameter(Mandatory = $true)][string]$TeamName,
+        [parameter(Mandatory = $true)][validateset("Public", "Private")][string]$TeamType,
+        [parameter(Mandatory = $true)][string]$TeamOwner,
+        [parameter(Mandatory = $false)][string]$TeamDescription
+    )
+    begin {
+        #Connecting to Teams:
+        Connect-MicrosoftTeams -Credential $ServiceAccountCredentials #-AccountID $env:ServiceAccountUsername -Verbose
+    }
+    Process {
+        #Generating the new team with some basic consistent settings
+        $TeamInfo = New-Team -DisplayName $TeamName -MailNickName $($TeamName.replace(" ", "")) -Owner $TeamOwner -Visibility $TeamType -Description $TeamDescription
+        #Outputting the results
+        $TeamInfo
+    }
+    End {
+        Disconnect-MicrosoftTeams
+    }
+}
+
+#Try to make the new team and report back the details and the status
+try {
+    $Results3 = New-MSTeams -TeamName $TempObject.TeamName -TeamType $TempObject.TeamType -TeamOwner $TempObject.TeamOwner
+    $Status = "Passed"
+}
+catch {
+    #This catch is activated if the function fails to run due to a bad or missing parameter, or a failed request to generate the new team
+    $results3 = $null
+    $Status = "Failed"
+}
+
+#Creating the table logging (needed table attributes)
+$TabbleLogging = [hashtable]@{  
+    partitionKey = 'TeamsLog'  
+    rowKey       = (new-guid).guid  
+}
+
+#Adding our temp attributes, the table attributes & the status of the group
+$Output = $TabbleLogging + $TempObject + @{
+    Status  = $Status
+    Results = $results3
+}
+
+#Converting to Json and pushing out to the host for humans to read
+Write-Host -message $($Output | convertto-json)
+
+#Writing to the table (for logging purposes)
+Push-OutputBinding -Name LoggedTeamInstalls -Value $Output
