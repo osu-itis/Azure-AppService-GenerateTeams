dba3d4a75fff3ac2773357fb8b6953d1b7fb6630
Keenan Carr <keenan.carr@oregonstate.edu>
2020-12-16 12:43:01 -0800

    Method to update group membership of teams management group
    This code allows us to update the membership of our teams management group. Discussing with Josh the pros and cons of what we've found with this method.

--- TimedGroupCheck/temp.ps1
+++ TimedGroupCheck/temp.ps1
@@ -28,3 +28,102 @@ do {
 
 # Now that we have all of the results, we can filter based on team (Right now this is our best option, there are some beta api calls to do this in a more simple way)
 $FilteredResults = $CollectedResults.Where({$_.resourceProvisioningOptions -like "*Team*"})
+
+# Setting the Names and IDs of our groups that we use for the management of Teams.
+$Management = @{
+    # This is our group for Exceptions
+    ExceptionsGroup = @{
+        Name = 'm365_exceptionteams'
+        ID = '15161ee5-a1a4-454b-a552-19ede6d66c13'
+    }
+    # This is our group of teams that we know of
+    KnownGroup = @{
+        Name = 'm365_knownteams'
+        ID = 'f514152c-b08d-4c32-97e9-21c7941dd699'
+    }
+}
+
+function ChunkSplitter {
+    <#
+    .SYNOPSIS
+    Split an array into chunks based on size
+
+    .DESCRIPTION
+    Split an array into chunks based on size
+
+    .PARAMETER chunkSize
+    Int of the size the array should be split into
+
+    .PARAMETER InputArray
+    The array to be split into chunks
+
+    .EXAMPLE
+    ChunkSplitter -ChunkSize 20 -InputArray $MyLongArray
+
+    .NOTES
+    This was created to split out long arrays into a short enough array that it can be used within Microsoft Graph API Calls
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [int]
+        $chunkSize,
+        [Parameter(Mandatory = $true)]
+        [array]
+        $InputArray
+    )
+
+    # Setting a generic outarray
+    $outArray = @()
+
+    # Determing the chunks needed based on the InputArray array
+    $parts = [math]::Ceiling($InputArray.Count / $chunkSize)
+
+    # Iterating through the array and splitting whenever we hit the max of the chunk
+    for ($i = 0; $i -le $parts; $i++) {
+        $start = $i * $chunkSize
+        $end = (($i + 1) * $chunkSize) - 1
+        $outArray += , @($InputArray[$start..$end])
+    }
+
+    # Outputting the results into the calculated chunks
+    $Output = $outArray | ForEach-Object { "$_" }
+
+    # Returning the Output of the function
+    return $Output
+}
+
+# Splitting the results into chunks of 20 to fit within Microsoft Graph's maximum number of IDs that can be added to a group at a time.
+$ChunkedResults = ChunkSplitter -chunkSize 20 -InputArray $FilteredResults.ID
+
+$GraphMembershipPatches = foreach ($Chunk in $ChunkedResults) {
+    # Splitting the chunk by spaces to have a clean array of the IDs
+    $Chunk = $Chunk.trim().Split(' ')
+
+    # Formatting the IDs into the needed directory object format
+    $DirectoryObjects = foreach ($ID in $Chunk) {
+        # If the ID is NOT empty, put it into Graph's directory object format
+        if (! [string]::IsNullOrEmpty($ID)) {
+            "https://graph.microsoft.com/v1.0/directoryObjects/$ID"
+        }
+    }
+
+    # A PSCustom Object containing the directory objects
+    [PSCustomObject]@{
+        "members@odata.bind" = $DirectoryObjects
+    }
+}
+
+foreach ($item in $GraphMembershipPatches) {
+    # Making a rest patch to to add the IDs as members of the group
+    $call = @{
+        Headers     = $Headers
+        Method      = 'Patch'
+        # Patching to our group of known Teams
+        URI         = "https://graph.microsoft.com/v1.0/groups/$($Management.KnownGroup.ID)"
+        ContentType = 'application/json'
+        # Converting our graph membership patch to json for the REST call
+        Body        = $($item | ConvertTo-Json)
+    }
+    Invoke-RestMethod @call
+}
