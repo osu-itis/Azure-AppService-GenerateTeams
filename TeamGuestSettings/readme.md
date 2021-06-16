abd3542aa6fb10d5daec6b505f0e021b893fc1fc
carrk-osu <69012336+carrk-osu@users.noreply.github.com>
2021-06-16 14:18:28 -0700

    Fix team guest settings (#26)
    * Added new graph group functions
    * Updated with current json output

--- TeamGuestSettings/readme.md
+++ TeamGuestSettings/readme.md
@@ -21,11 +21,9 @@ Host: <HOST>
 
 ```JSON
 {
-    "id": "4172d260-9f5b-4a90-8ec2-55315a4b5874",
-    "mail": "KeenanTestingTeams1171034411@OregonStateUniversity.onmicrosoft.com",
-    "displayName": "Keenan Testing Teams",
-    "AllowToAddGuests": "true",
-    "GuestSettingsID": "998624a0-1a8c-4201-8b3c-d7718ff26c4f"
+    "DisplayName": "Keenan Testing Teams",
+    "ID": "4172d260-9f5b-4a90-8ec2-55315a4b5874",
+    "AllowToAddGuests": "true"
 }
 ```
 
@@ -48,10 +46,8 @@ Content-Type: application/json
 
 ```JSON
 {
-    "id": "4172d260-9f5b-4a90-8ec2-55315a4b5874",
-    "mail": "KeenanTestingTeams1171034411@OregonStateUniversity.onmicrosoft.com",
-    "displayName": "Keenan Testing Teams",
-    "AllowToAddGuests": "true",
-    "GuestSettingsID": "998624a0-1a8c-4201-8b3c-d7718ff26c4f"
+    "DisplayName": "Keenan Testing Teams",
+    "ID": "4172d260-9f5b-4a90-8ec2-55315a4b5874",
+    "AllowToAddGuests": "true"
 }
 ```
--- TeamGuestSettings/run.ps1
+++ TeamGuestSettings/run.ps1
@@ -16,108 +16,17 @@ import-module .\Modules\New-GraphAPIToken\New-GraphAPIToken.psm1
 $GraphAPIToken = New-GraphAPIToken -ClientID $env:ClientID -ClientSecret $env:ClientSecret -TenantID $env:TenantID
 $Headers = $GraphAPIToken.Headers
 
-# Defining functions
-function Get-TeamInfo {
-    <#
-    .SYNOPSIS
-    Gathers information about the guest access of a team
-
-    .DESCRIPTION
-    Gathers information about the guest access of a team
-
-    .PARAMETER TeamName
-    The display name of the team to inspect
-
-    .EXAMPLE
-    $GuestSettings = get-teamInfo -teamName "some team name"
-    #>
-    param ($TeamName)
-
-    # Get team general info
-    $URI = 'https://graph.microsoft.com/v1.0/groups?$filter=startswith(displayName,' + "'"+$TeamName+"')"
-    $Results = Invoke-RestMethod -Headers $Headers -Method Get -Uri $URI
-    $TeamInfo = $Results.value|Select-Object ID,Mail,displayname
-
-    # Get current team guest access setting
-    $results = Invoke-RestMethod -headers $Headers -Method get -uri $(
-        "https://graph.microsoft.com/v1.0/groups/"+$TeamInfo.id+"/settings"
-    )
-
-    Add-Member -InputObject $TeamInfo -NotePropertyName "AllowToAddGuests" -NotePropertyValue $Results.value.values.value
-    Add-Member -InputObject $TeamInfo -NotePropertyName "GuestSettingsID" -NotePropertyValue $Results.value.id
-    Return $TeamInfo
-}
-
-function Set-TeamInfo {
-    <#
-    .SYNOPSIS
-    Sets the AllowToAddGuests setting
-
-    .DESCRIPTION
-    Sets the AllowToAddGuests setting
-
-    .PARAMETER Headers
-    The headers to make the post/patch request to the graph api
-
-    .PARAMETER GroupID
-    The ID of the group (team) that is to be modified
-
-    .PARAMETER value
-    The true/false value in string format
-
-    .EXAMPLE
-    Set-TeamInfo -Headers $Headers -GroupID <GUID> -value true
-    #>
-    [CmdletBinding()]
-    param (
-        $Headers,
-        $GroupID,
-        $value
-    )
-
-    $Func = [PSCustomObject]@{
-        Headers = $Headers
-        GroupID = $GroupID
-        body = "{
-            `n    `"displayName`": `"GroupSettings`",
-            `n    `"templateId`": `"08d542b9-071f-4e16-94b0-74abb372e3d9`",
-            `n    `"values`": [
-            `n        {
-            `n            `"name`": `"AllowToAddGuests`",
-            `n            `"value`": `"$value`"
-            `n        }
-            `n    ]
-            `n}"
-    }
-
-        $GroupInfo = Invoke-RestMethod -Uri "https://graph.microsoft.com/v1.0/groups/$($Func.GroupID)" -Method "get" -Headers $func.Headers
-        $GroupSettingsInfo = Invoke-RestMethod -Uri "https://graph.microsoft.com/v1.0/groups/$($Func.GroupID)/settings" -Method "get" -Headers $func.Headers
-
-    try {
-        #Try to post the information first. If the setting has never been applied this will add it
-        Write-Output "Attempting post to settings"
-        Invoke-RestMethod -Uri "https://graph.microsoft.com/v1.0/groups/$($Func.GroupID)/settings" -Method "post" -Headers $func.Headers -Body $func.body -ContentType 'application/json'
-    }
-    catch {
-        try {
-            #If posting fails, try patching the information since the setting already exists on the object
-            Write-Output "Attempting patch to settings"
-            Invoke-RestMethod "https://graph.microsoft.com/v1.0/groups/$($func.GroupID)/settings/$($GroupSettingsInfo.value.id)" -Method 'patch' -Body $func.body -ContentType 'application/json'  -Headers $Headers
-        }
-        catch {
-            #Sometimes the resource is not available and needs to be tried again in a few moments
-            start-sleep -Seconds 5
-            Write-Output "Attempting patch to settings"
-            Invoke-RestMethod "https://graph.microsoft.com/v1.0/groups/$($func.GroupID)/settings/$($GroupSettingsInfo.value.id)" -Method 'patch' -Body $func.body -ContentType 'application/json'  -Headers $Headers
-        }
-    }
-}
+# Importing Functions
+Write-output "Importing functions"
+Import-Module .\Modules\GraphGroupFunctions\GraphGroupFunctions.psm1
 
 # Determine if this is a get or a post, and gather the request to process.
-switch ($request.Method)
-{
+switch ($Request.Method) {
     'Get' {
+        # Setting the request to process
         $RequestToProcess = $Request.Query
+
+        # If a teamname was not provided, return badrequest
         if (-not $RequestToProcess.TeamName) {
             Write-Output "Sending bad request response"
             Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
@@ -127,7 +36,34 @@ switch ($request.Method)
             break
         }
         Write-output "Gathering team info"
-        $body = $(get-teamInfo -teamName $RequestToProcess.TeamName)
+
+        # Getting the name of the group to find
+        $TeamName = $RequestToProcess.TeamName
+
+        # Getting the template
+        $GuestTemplate = Get-GraphGroupGuestTemplate -Headers $Headers
+
+        # Getting the graph group
+        $Group = Get-GraphGroup -Headers $Headers -TeamName $TeamName
+
+        try {
+            # Getting the graph group settings
+            $Settings = Get-GraphGroupGuestSettings -Headers $Headers -GroupID $group.id
+        }
+        catch {
+            Write-Output "Sending bad request response"
+            Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+                StatusCode = [HttpStatusCode]::BadRequest
+                Body = $("Could not find group settings for $teamname")
+            })
+            break
+        }
+
+        $body = [PSCustomObject]@{
+            DisplayName = $group.displayName
+            ID = $group.ID
+            AllowToAddGuests = $Settings.values.value
+        }
 
         # Associate values to output bindings by calling 'Push-OutputBinding'.
         Write-Output "Sending response"
@@ -137,7 +73,10 @@ switch ($request.Method)
         })
     }
     'Post' {
+        # Setting the request to process
         $RequestToProcess = $Request.Body
+
+        #If a teamname was not provided, respond with badrequest
         if (-not $RequestToProcess.TeamName) {
             Write-Output "Sending bad request response"
             Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
@@ -146,14 +85,49 @@ switch ($request.Method)
             })
             break
         }
+
+        #If a non-boolean value was provided, respond with badrequest
+        if (($RequestToProcess.GuestSettingsEnabled -ne "true") -and ($RequestToProcess.GuestSettingsEnabled -ne "false")) {
+            Write-Output "Sending bad request response"
+            Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+                StatusCode = [HttpStatusCode]::BadRequest
+                Body = $("Request not formatted properly")
+            })
+            break
+        }
+
+
         Write-Output "Request: $($RequestToProcess|ConvertTo-Json)"
         Write-Output "Processing request for ticket $($RequestToProcess.TicketID)"
         Write-output "Gathering team info"
-        $GuestSettings = get-teamInfo -teamName $RequestToProcess.TeamName
-        Write-output "Setting team info"
-        Set-TeamInfo -Headers $Headers -GroupID $GuestSettings.id -value $RequestToProcess.GuestSettingsEnabled
-        Write-output "Gathering team info"
-        $Body = get-teamInfo -teamName $RequestToProcess.TeamName
+        # Getting the name of the group to find
+        $TeamName = $RequestToProcess.TeamName
+
+        # Getting the template
+        $GuestTemplate = Get-GraphGroupGuestTemplate -Headers $Headers
+
+        # Getting the graph group
+        $Group = Get-GraphGroup -Headers $Headers -TeamName $TeamName
+
+        try {
+            # Setting the graph group settings
+            Write-output "Setting team info"
+            $Settings = Set-GraphGroupGuestSettings -Headers $Headers -GroupUnifiedGuestTemplateID $GuestTemplate.id -GroupID $Group.id -AllowToAddGuests $RequestToProcess.GuestSettingsEnabled
+        }
+        catch {
+            Write-Output "Sending bad request response"
+            Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+                StatusCode = [HttpStatusCode]::BadRequest
+                Body = $("Could not find group settings for $teamname")
+            })
+            break
+        }
+
+        $body = [PSCustomObject]@{
+            DisplayName = $group.displayName
+            ID = $group.ID
+            AllowToAddGuests = $Settings.values.value
+        }
 
         # Associate values to output bindings by calling 'Push-OutputBinding'.
         Write-Output "Sending response"
